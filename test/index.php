b68b0c051d1a38da97fe1cb22147ca8611f97761
nipuni1997 <54950127+nipuni1997@users.noreply.github.com>
2020-10-08 09:35:42 +0530

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title>One Cart 
+Online Shopping</title>
+<link rel="stylesheet" href="css/style.css" />
+<link href="https://fonts.googleapis.com/css2?family=Lemonada:wght@600&display=swap" rel="stylesheet">
+
+</head>
+
+
+<body>
+<div id="header">
+<div id="logo">
+  <img src="img/logo.png" />
+
+</div> <!-- end of logo -->
+<div id="name">
+<h1>ONE CART ONLINE SHOPPING</h1>
+
+</div><!-- end of name -->
+<div id="link">
+   <ul>
+     <li><a href="#">Signup</a></li>
+     <li><a href="#">Login</a></li>
+   </ul>
+
+
+</div> <!-- end of link -->
+</div><!-- end of header -->
+<div id="navbar">
+    <ul>
+        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Home</a></li>
+    </ul>
+</div>
+<div id="search">
+                <form method="get" enctype="multipart/form-data">
+                    <input type="text" placeholder="Search From Here...">
+                    <button id="search_btn">Search</button>
+                    <button id="cart_btn">Cart</button>
+                </form>
+            </div><!--end of search-->
+
+</body>
+
+
+</html>
